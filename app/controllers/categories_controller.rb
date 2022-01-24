8fd3830f246e816539dacfddb23b497f82284c51
Alberto <aiduma08@gmail.com>
2022-01-24 15:09:10 -0700

    Integration test and feature: listing categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,8 @@ class CategoriesController < ApplicationController
     end
     
     def index
-        
+      ## perform a paginated query:
+      @categories = Category.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     end
 
     def show
