2e9e38714c7f5b34f4f4b61a3a35a244610e5c5d
Alberto <aiduma08@gmail.com>
2022-01-25 13:35:35 -0700

    Update article views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,6 +22,7 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.order(created_at: :desc).paginate(page: params[:page], per_page: 5)	
     end
 
     private
