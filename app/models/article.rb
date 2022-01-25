41d9bbcab86d929816a606dfebde2208e77a555b
Alberto <aiduma08@gmail.com>
2022-01-25 09:35:25 -0700

    Many-to-many association - implementation

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,8 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 6, maximum: 100}
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
+
+  has_many :article_categories
+  has_many :categories, through: :article_categories  
 end
 
